f782e85bec227f94085c69d453a3b0a90c07467b
Jacek Jabłonka <jacjab@gmail.com>
2016-01-06 11:12:55 +0100

    JUnit and spring-test config - without Spring Context yet.

---
+++ src/test/java/pl/liquidkit/springinaction4/chapter02/di/soundsystem/CDPlayerTest.java
@@ -0,0 +1,17 @@
+package pl.liquidkit.springinaction4.chapter02.di.soundsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Jacek on 06.01.2016.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CDPlayerTest {
+
+    @Test
+    public void cdShouldNotBeNull() {
+
+    }
+}
